35cf01676b99f54dcafc61a536116e2c6dd3f16e
riceball-tw <42161586+riceball-tw@users.noreply.github.com>
2025-04-09 02:29:23 +0800

    feat: Apply breed image list component

--- src/app/breed/[...breedName]/page.tsx
+++ src/app/breed/[...breedName]/page.tsx
@@ -1,7 +1,6 @@
-
 import { getBreeds, getFlattenBreeds, getImagesFromBreed } from '@/utility/breeds'
 import type { Metadata } from 'next'
-import Image from 'next/image'
+import BreedImageList from '@/components/atoms/BreedImageList'
 
 interface Query {
   params: Promise<{
@@ -13,37 +12,25 @@ export async function generateStaticParams() {
   const breeds = await getBreeds()
   if (!breeds) throw Error('Could not get breeds')
   const flatBreeds = getFlattenBreeds(breeds)
-  return [
-    flatBreeds
-  ]
+  return [flatBreeds]
 }
 
 export async function generateMetadata({ params }: Query): Promise<Metadata> {
   const { breedName } = await params
-  const title = Array.isArray(breedName) ? breedName.join('/') : breedName;
-  return {
-    title,
-  };
+  const title = Array.isArray(breedName) ? breedName.join('/') : breedName
+  return { title }
 }
 
 export default async function BreedPage({ params }: Query) {
-  const { breedName } = await params;
+  const { breedName } = await params
   const breedPath = breedName.join('/')
-  const data = await getImagesFromBreed(breedPath)
-  if (!data) return
+  const imageRes = await getImagesFromBreed(breedPath)
+  if (!imageRes) return
+
   return (
-    <div className="grid grid-cols-3 gap-1 p-3">
-      {data.message.map((url, index) => (
-        <button key={index} type="button">
-          <Image
-            className="bg-global-image-fallback w-full aspect-square object-cover"
-            width="95"
-            height="95"
-            src={url}
-            alt={breedPath}
-          />
-        </button>
-      ))}
-    </div>
+    <BreedImageList
+      imageUrls={imageRes.message}
+      breedPath={breedPath}
+    />
   )
 }
---
+++ src/components/atoms/BreedImageList.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from "react";
+import Image from 'next/image'
+import Carousel from '@/components/atoms/Carousel'
+import Dialog from '@/components/molecules/Dialog'
+
+interface BreedImageListProps {
+  imageUrls: string[]
+  breedPath: string
+}
+
+export default function BreedImageList({ imageUrls, breedPath }: BreedImageListProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false)
+  const [selectedUrl, setSelectedUrl] = useState<string | null>(null)
+
+  const imageUrlsWithoutSelected = imageUrls.filter(imageUrl => {
+    return imageUrl !== selectedUrl 
+  })
+  const carouselImages = imageUrlsWithoutSelected.map(url => {
+    return {
+      url,
+      alt: breedPath
+    }
+  })
+
+  return (
+    <>
+      <Dialog isDialogOpen={isDialogOpen} setIsDialogOpen={setIsDialogOpen}>
+        <Carousel images={selectedUrl ? carouselImages: []} width={250} height={250} />
+      </Dialog>
+
+      <div className="grid grid-cols-3 gap-1 p-3">
+        {imageUrls.map((url, index) => (
+          <button
+            key={index}
+            type="button"
+            onClick={() => {
+              setSelectedUrl(url)
+              setIsDialogOpen(true)
+            }}
+            className="cursor-pointer"
+          >
+            <Image
+              className="bg-global-image-fallback w-full aspect-square object-cover"
+              width={95}
+              height={95}
+              src={url}
+              alt={breedPath}
+            />
+          </button>
+        ))}
+      </div>
+    </>
+  )
+}
